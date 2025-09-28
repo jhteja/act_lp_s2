226bf5f748426eeb91f7816225ff0c3cfe1a40b0
Jaime T <50801922+jhteja@users.noreply.github.com>
2025-09-28 17:37:18 -0500

    Add files via upload

---
+++ target/classes/com/example/Ejercicio1.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Ejercicio1 {
+    public static void ejercicio1() {
+        System.out.println("Ejercicio 1");
+        int edad = 25;
+        double altura = 1.75;
+        String nombre = "Jaime Hernán";
+        boolean esEstudiante = true;
+        System.out.println("Edad: " + edad + ", Altura: " + altura + ", Nombre: " + nombre + ", Es estudiante: " + esEstudiante);
+    }
+}
